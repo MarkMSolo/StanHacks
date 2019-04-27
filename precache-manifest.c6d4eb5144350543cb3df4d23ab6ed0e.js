738b310f8641c05535d16026db2b50c9242c66b7
John Solo <johnsolojoseph@gmail.com>
2019-04-27 15:20:14 -0700

    Updates

---
+++ precache-manifest.c6d4eb5144350543cb3df4d23ab6ed0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ae599b7dfdf2e17ed56",
+    "url": "/StanHacks/static/js/runtime~main.3ae599b7.js"
+  },
+  {
+    "revision": "79b03d4e69abf5a17d2b",
+    "url": "/StanHacks/static/js/main.79b03d4e.chunk.js"
+  },
+  {
+    "revision": "85bbab274fae31a80bed",
+    "url": "/StanHacks/static/js/1.85bbab27.chunk.js"
+  },
+  {
+    "revision": "79b03d4e69abf5a17d2b",
+    "url": "/StanHacks/static/css/main.888fb655.chunk.css"
+  },
+  {
+    "revision": "7475912c6f7fac64127951a13c0f1a5e",
+    "url": "/StanHacks/index.html"
+  }
+];
